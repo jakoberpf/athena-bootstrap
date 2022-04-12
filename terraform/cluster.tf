b51591f39d667b71e1e472fff1291453c8d846ef
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-12 14:53:43 +0200

    update cluster config

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -6,16 +6,15 @@ module "kubernetes_cluster" {
     zerotier = zerotier
   }
 
-  id_prefix              = 20
-  name                   = "athena"
-  node                   = "dev"
-  instance_template      = "ubuntu-focal-cloudinit"
-  instance_cpus          = 4
-  instance_memory        = 16384
-  instance_disk_backend  = "local-zfs"
-  network_ip_prefix      = "192.168.0.10"
-  network_gateway        = "192.168.0.1"
-  ssh_authorized_keys    = var.authorized_keys
-  zerotier_network_id    = var.zerotier_network_id_development
-  zerotier_ip_assignment = "10.147.19.110"
+  id_prefix             = 20
+  name                  = "athena"
+  node                  = "dev"
+  instance_template     = "ubuntu-focal-cloudinit"
+  instance_cpus         = 4
+  instance_memory       = 16384
+  instance_disk_backend = "local-zfs"
+  network_ip_prefix     = "192.168.0.10"
+  network_gateway       = "192.168.0.1"
+  ssh_authorized_keys   = var.authorized_keys
+  zerotier_subnet       = "10.110.180.0"
 }
