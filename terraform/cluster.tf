7054b95d5efd986ae4a0a98272512e39bf551dfa
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-05-08 12:32:46 +0200

    update cluster

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -8,20 +8,28 @@ module "kubernetes_cluster" {
     zerotier       = zerotier
   }
 
-  id                    = 20
-  name                  = "athena"
-  node                  = "dev"
-  instance_template     = "ubuntu-focal-cloudinit"
-  instance_cpus         = 4
-  instance_memory       = 16384
-  instance_disk_backend = "local-zfs"
-  network_ip_prefix     = "192.168.0.10"
-  network_gateway       = "192.168.0.1"
-  ssh_authorized_keys   = var.authorized_keys
-  zerotier_subnet       = "10.110.180.0"
-  bastion_host          = "10.147.19.60"
-  bastion_user          = "root"
-  cloudflare_email      = var.cloudflare_email
-  cloudflare_zone_id    = var.cloudflare_zone_id
-  cloudflare_token      = var.cloudflare_token
+  id                           = 20
+  name                         = "athena"
+  node                         = "dev"
+  count_masters                = 3
+  master_instance_template     = "ubuntu-focal-cloudinit"
+  master_instance_cpus         = 2
+  master_instance_memory       = 8000
+  master_instance_disk_backend = "vm-storage"
+  master_instance_disk_size    = "19660M"
+  count_workers                = 3
+  worker_instance_template     = "ubuntu-focal-cloudinit"
+  worker_instance_cpus         = 4
+  worker_instance_memory       = 16000
+  worker_instance_disk_backend = "vm-storage"
+  worker_instance_disk_size    = "39116M"
+  network_ip_prefix            = "192.168.0.1"
+  network_gateway              = "192.168.0.1"
+  ssh_authorized_keys          = var.authorized_keys
+  zerotier_subnet              = "10.110.180.0"
+  bastion_host                 = "10.147.19.60"
+  bastion_user                 = "root"
+  cloudflare_email             = var.cloudflare_email
+  cloudflare_zone_id           = var.cloudflare_zone_id
+  cloudflare_token             = var.cloudflare_token
 }
